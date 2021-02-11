4373ad6f469d6e74e45d5564d90e7cfe298b2722
DembaMBOW <demba.pro.76000@gmail.com>
2021-02-11 10:27:49 +0100

    Intégration style footer et changement liens logo footer

--- wp-content/themes/lifepropulse/lifepropulse/footer.php
+++ wp-content/themes/lifepropulse/lifepropulse/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -9,11 +10,11 @@
  * @package lifepropulse
  */
 $args3 = array(
-    'post_type' => 'tweet',
-    'post_status' => 'publish',
-    'posts_per_page' => 5,
-    'orderby' => 'date',
-    'order' => 'DESC'
+	'post_type' => 'tweet',
+	'post_status' => 'publish',
+	'posts_per_page' => 5,
+	'orderby' => 'date',
+	'order' => 'DESC'
 );
 
 $the_query3 = new WP_Query($args3);
@@ -23,70 +24,42 @@ $the_query3 = new WP_Query($args3);
 
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
+<footer id="l-footer colophon" class="site-footer">
+	<div class="wrap">
+		<div class="flex">
+			<div class="logo-footer_center">
+				<div class="logo">
+					<a href="#"><img src="<?php echo get_template_directory_uri() ?> /asset/img/logo_footer.png" alt=""></a>
+				</div>
+				<div class="contact_footer">
+					<p>E-Mail contact@lifepropulse.fr</p>
+					<p>Telephone 02 32 00 00 00</p>
+				</div>
+			</div>
+			<ul>
+				<div class="links">
 
+					<li><a href="#">Mention Légales</a></li>
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">Contact</a></li>
+				</div>
+				<div class="social">
 
-    <footer id="l-footer">
-        <div class="wrap">
-            <div class="flex">
-                <div class="logo-footer_center">
-                    <div class="logo">
-                        <a href="#"><img src="./asset/img/logo_footer.png" alt=""></a>
-                    </div>
-                    <div class="contact_footer">
-                        <p>E-Mail contact@lifepropulse.fr</p>
-                        <p>Telephone 02 32 00 00 00</p>
-                    </div>
-                </div>
-                <ul>
-                    <div class="links">
-
-                        <li><a href="#">Mention Légales</a></li>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </div>
-                    <div class="social">
-
-                        <li><a href="#">Logo Facebook</a></li>
-                        <li><a href="#">Logo Twitter</a></li>
-                        <li><a href="#">Logo Instagram</a></li>
-                        <li><a href="#">Logo Linkedn</a></li>
-                    </div>
-                </ul>
-            </div>
-        </div>
-        <div class="footer_foot">
-            <div class="wrap">
-                <p> @COPYRIGHT LIFE PROPULSE 2021 | TOUS DROITS RESERVES</p>
-            </div>
-        </div>
-    </footer>
-	<?php wp_footer(); ?>
-</html>
-
-
-
-
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lifepropulse' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'lifepropulse' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lifepropulse' ), 'lifepropulse', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->		
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+					<li><a href="#">Logo Facebook</a></li>
+					<li><a href="#">Logo Twitter</a></li>
+					<li><a href="#">Logo Instagram</a></li>
+					<li><a href="#">Logo Linkedn</a></li>
+				</div>
+			</ul>
+		</div>
+	</div>
+	<div class="footer_foot">
+		<div class="wrap">
+			<p> @COPYRIGHT LIFE PROPULSE 2021 | TOUS DROITS RESERVES</p>
+		</div>
+	</div>
+</footer>
 <?php wp_footer(); ?>
-
 </body>
+
 </html>
