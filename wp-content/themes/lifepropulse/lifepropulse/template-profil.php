dd9704c51eb80064e812bbfa0b3f7c10662f9114
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2021-02-15 12:43:11 +0100

    securisation et validation du formulaire contact + mise en bdd des messages

--- wp-content/themes/lifepropulse/lifepropulse/template-profil.php
+++ wp-content/themes/lifepropulse/lifepropulse/template-profil.php
@@ -10,16 +10,14 @@ if($user->ID == 0){
 get_header();
 ?>
 
-<section>
-    <h1>Mes Informations</h1>
+<section class="site-main">
+    <h1 class="h1-page-title">Mes Informations</h1>
 
     <p>Bonjour <?= $user->display_name; ?> </p>
     <p>Votre email : <?= $user->user_email; ?> </p>
     <p>Premiére connexion : <?= $user->user_registered; ?> </p>
 
 </section>
-
-
 
 <?php
 get_footer();
